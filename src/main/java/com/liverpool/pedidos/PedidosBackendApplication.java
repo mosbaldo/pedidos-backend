e53b7a1374d5f4700e4aa541b35071015d6e4371
Miguel Osbaldo Gallardo Toledo <osbald91@gmail.com>
2026-08-13 22:21:58 -0600

    chore: :tada: inicialización del arquetipo y configuración base

---
+++ src/main/java/com/liverpool/pedidos/PedidosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.liverpool.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liverpool/pedidos/PedidosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liverpool.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
